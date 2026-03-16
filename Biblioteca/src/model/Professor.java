991a25ad9b8484bc33dcd4bfd815fcd22283f489
Munir Zenni <dtntud8@gmail.com>
2026-03-16 09:18:49 -0300

    Delete Biblioteca directory

--- Biblioteca/src/model/Professor.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Professor extends Usuario {
-    public Professor(String nome, String matricula, String endereco) {
-        super(nome, matricula, endereco, "Professor");
-    }
-}
