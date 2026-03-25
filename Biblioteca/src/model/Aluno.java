fd51d8a73fd104c7455cbc1bf3cab48ad4a73704
Munir Zenni <dtntud8@gmail.com>
2026-03-25 10:48:05 -0300

    Delete Biblioteca directory

--- Biblioteca/src/model/Aluno.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Aluno extends Usuario {
-    public Aluno(String nome, String matricula, String endereco) {
-        super(nome, matricula, endereco, "Aluno");
-    }
-}
--- Biblioteca/src/model/Funcionario.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Funcionario extends Usuario {
-    public Funcionario(String nome, String matricula, String endereco) {
-        super(nome, matricula, endereco, "Funcionario");
-    }
-}
